755d2429be2c9ab97c75a26fd4252cd19ab07208
Eric Tai <eric.loong@sgcodecampus.com>
2019-11-25 11:50:03 +0800

    Added more resolvers to showcase generic resolvers' use cases

--- app.js
+++ app.js
@@ -6,11 +6,27 @@ const { buildSchema } = require('graphql');
 const schema = buildSchema(`
   type Query {
     hello: String!
+    ip: String!
+    rollOneDice(numSides: Int!): Int!
+    rollOneSixSidedDice: Int!
+    rollDice(numSides: Int, numDice: Int!): [Int]!
   }
 `);
 
 const rootValue = {
   hello: () => 'Hello World',
+  ip: (args, req) => req.ip,
+  rollOneDice: ({ numSides }) => Math.floor(Math.random() * numSides) + 1,
+  rollOneSixSidedDice() {
+    return this.rollOneDice({ numSides: 6 });
+  },
+  rollDice({ numSides = 6, numDice }) {
+    const rolls = [];
+    for (let i = 0; i < numDice; i += 1) {
+      rolls.push(this.rollOneDice({ numSides }));
+    }
+    return rolls;
+  },
 };
 
 const app = express();
