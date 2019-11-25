8ee28616e36f601589d9ca272cf33320786a2a30
Eric Tai <eric.loong@sgcodecampus.com>
2019-11-25 10:34:38 +0800

    Added initial eslint settings. Added basic GraphQL functionality

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+  type Query {
+    hello: String!
+  }
+`);
+
+const rootValue = {
+  hello: () => 'Hello World',
+};
+
+const app = express();
+app.use(cors());
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue,
+    graphiql: true,
+  }),
+);
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`App is now listening to port ${process.env.port || 4000} `);
+});
