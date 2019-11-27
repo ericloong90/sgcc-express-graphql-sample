d93bfdc6c0f7800eb3757381c29d25280f0d3a49
Eric Tai <eric.loong@sgcodecampus.com>
2019-11-27 13:21:11 +0800

    Showing all fields in the callback argument in the extensions object

--- extensions/index.js
+++ extensions/index.js
@@ -1,5 +1,15 @@
 const runTime = require('./runTime');
 
-module.exports = ({ context: { startTime } }) => ({
-  runTime: runTime(startTime),
-});
+module.exports = ({
+  context: { startTime },
+  variables,
+  document,
+  operationName,
+  result,
+}) => {
+  console.log('variables: ', variables);
+
+  return {
+    runTime: runTime(startTime),
+  };
+};
